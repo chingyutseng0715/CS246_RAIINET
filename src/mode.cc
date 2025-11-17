6d6151790ece83262abc4d1a4dda1a0cc9399ac8
ChingYuTseng <bloodkill950715@gmail.com>
2025-11-16 19:18:30 -0500

    public methods listed 3.0

--- src/mode.cc
+++ src/mode.cc
@@ -1,6 +1,12 @@
 export module Mode;
 
 export class Mode {
+  protected:
+    Board board;
+    std::vector<Player> players;
+    int num_players;
+
   public:
     virtual void operatingGame() = 0;
+  
 };

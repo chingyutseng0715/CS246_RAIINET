e1a32af4b9de233733d140c175bf7e90cd694418
ChingYuTseng <bloodkill950715@gmail.com>
2025-11-23 16:50:48 -0500

    High Transmissible Virus ability implementation

---
+++ src/high-virus-impl.cc
@@ -0,0 +1,25 @@
+module highVirus;
+import Board;
+import Ability;
+import Link;
+import <sstream>;
+import <string>;
+import <stdexcept>;
+
+highVirus::highVirus(Observer *owner, Board *board)
+    : Ability{"High Transmissible Virus", "Attach to virus, when infected make downloader's downloaded data become virus, if no data change one of it's virus on board to data, it no virus then the ability is directly mark used", owner, board} {}
+
+void highVirus::operatingAbility(std::string command) {
+	std::istringstream iss(command);
+	char link_char;
+	
+	if (!(iss >> link_char)) {
+		throw std::invalid_argument("Invalid command for high transmissible virus ability");
+	}
+	
+	Link* target_link = board->getLink(link_char);
+	if (target_link && target_link->getPlayer() == player && target_link->getType() == "Virus") {
+		target_link->setInfected(true);
+		markUsed();
+	}
+}

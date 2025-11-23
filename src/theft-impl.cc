577edaa6ee84d478ac660605803bcb62b7d7f2d4
ChingYuTseng <bloodkill950715@gmail.com>
2025-11-22 21:40:34 -0500

    random theft ability implementation

--- src/theft-impl.cc
+++ src/theft-impl.cc
@@ -2,6 +2,7 @@ module Theft;
 import <string>;
 import <sstream>;
 import <stdexcept>;
+import <random>;
 import Ability;
 import Board;
 import Link;
@@ -9,20 +10,31 @@ import Link;
 Theft::Theft(Observer *owner, Board *board)
 	: Ability{"Theft", "Steal an assigned other player's ability", owner, board} {}
 
-void Thief::operatingAbility(std::string command) {
+void Theft::operatingAbility(std::string command) {
 	std::istringstream iss(command);
 	int player_index;
-	if (!(iss >> player_index)) {
+	iss >> player_index;
+
+    if (!(iss >> player_index)) {
 		throw std::invalid_argument("Invalid command for Theft ability");
 	}
 	
 	Observer *opponent = board->getPlayer(player_index);
 	
 	if (opponent && opponent != player) {
-		Ability *stolen_ability = board->getRandomAbility(opponent);
+		int ability_count = opponent->getAbilityAmount();
 		
-		if (stolen_ability) {
-			player->addAbility(*stolen_ability);
+		if (ability_count > 0) {
+
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<> dis(0, ability_count - 1);
+			int random_index = dis(gen);
+			
+			Ability &stolen_ability = opponent->getAbilityAt(random_index);
+			player->addAbility(stolen_ability);
+			opponent->removeAbility(random_index);
+			markUsed();
 		}
 	}
 }
